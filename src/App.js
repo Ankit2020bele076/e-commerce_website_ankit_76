2d1cd73f8dd4820cfdac9124c7cd4091c0786639
Ankit2020bele076 <ankitpayal160@gmail.com>
2024-11-22 19:20:00 +0530

    added product info card and ratings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/navigation/navigation';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import HomePage from './customer/Pages/HomePage/HomePage';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Navigation />
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        <ProductDetails />
       </div>
       <Footer />
     </div>
