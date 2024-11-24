f19ea58e025519cd2b43a0e79fc9d92b2f1c9897
Ankit2020bele076 <ankitpayal160@gmail.com>
2024-11-24 12:04:42 +0530

    added Order and OrderCard page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/navigation/navigation';
+import Order from './customer/components/Order/Order';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import HomePage from './customer/Pages/HomePage/HomePage';
@@ -16,7 +17,8 @@ function App() {
         {/* <Product /> */}
         {/* <ProductDetails /> */}
         {/* <Cart /> */}
-        <Checkout />
+        {/* <Checkout /> */}
+        <Order />
       </div>
       <Footer />
     </div>
